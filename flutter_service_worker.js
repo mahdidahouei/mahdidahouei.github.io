690f9a8a77fd2d770b31049b9c9d421edf99aab8
MahdiArabpour <mahdi.a.200066@gmail.com>
2023-09-28 00:26:53 +0330

    updated description

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "9b818ca9511483c901bed1545384376c",
-"index.html": "47a37dfe46e0f94b7c05da796fb86538",
-"/": "47a37dfe46e0f94b7c05da796fb86538",
+"index.html": "856e51f31a380c70657b27d55fb7efdb",
+"/": "856e51f31a380c70657b27d55fb7efdb",
 "CNAME": "aa9fb5c895f59df0fceeddab82ad73cf",
 "main.dart.js": "1adde4daeb9db9b219b7abe863633750",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
